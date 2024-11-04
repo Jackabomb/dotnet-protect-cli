668fa8f9af4242bd7deba392b999ba950938a49b
Jackabomb <48334975+Jackabomb@users.noreply.github.com>
2024-11-04 11:40:31 -0700

    Import project.

---
+++ Reprotect/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+Console.WriteLine(Convert.ToBase64String(ProtectedData.Protect(Convert.FromBase64String(Console.ReadLine()), null, DataProtectionScope.CurrentUser)));
---
+++ Unprotect/Program.cs
@@ -0,0 +1,4 @@
+﻿using System;
+using System.Security.Cryptography;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine(Convert.ToBase64String(ProtectedData.Unprotect(Convert.FromBase64String(Console.ReadLine()), null, DataProtectionScope.CurrentUser)));
